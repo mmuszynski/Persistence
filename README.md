665c79736263bbcb3b517f65f390703c4b10187d
Mike Muszynski <mmuszynski@gmail.com>
2023-03-01 13:33:31 -0500

    First commit with readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Persistence
 
-A description of this package.
+I got tired of having to check to see if the application support directory was available before using it. Or rewriting for the hundredth time FileManager.default.fileExistsAt a directory that needs to be initialized before it can be used. This is my attempt at fixing a lot of that headache.
--- Sources/Persistence/Persistence.swift
+++ Sources/Persistence/Persistence.swift
@@ -1,6 +1,40 @@
-public struct Persistence {
-    public private(set) var text = "Hello, World!"
+/// This project is my attempt to wrap persistence information into a useable format
+/// Basically, after the millionth time creating ApplicationSupport directories, I want something that is faster
+///
 
-    public init() {
+import Foundation
+
+public struct Persistence {
+    struct Location {
+        enum LocationError: Error {
+            case noLocationNoCreate
+        }
+        
+        var baseURL: URL
+        
+        static var userApplicationSupport: Self {
+            get throws {
+                let url = try FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+                return Self(baseURL: url)
+            }
+        }
+        
+        static func userApplicationSupport(appending path: String, createIfNeeded: Bool = false) throws -> Self {
+            let fullURL: URL
+            if #available(macOS 13.0, *) {
+                fullURL = try Self.userApplicationSupport.baseURL.appending(path: path)
+            } else {
+                fullURL = try Self.userApplicationSupport.baseURL.appendingPathComponent(path)
+            }
+            if !FileManager.default.fileExists(atPath: fullURL.absoluteString) {
+                if createIfNeeded == false {
+                    throw LocationError.noLocationNoCreate
+                }
+                
+                try FileManager.default.createDirectory(at: fullURL, withIntermediateDirectories: true)
+            }
+            
+            return Self(baseURL: fullURL)
+        }
     }
 }
